564273c5fe22d954def62330a9cea8752c3e0530
fiso64 <sokfilix@gmail.com>
2025-11-19 20:31:31 +0100

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -58,8 +58,8 @@ namespace MusicBeePlugin
             about.TargetApplication = "";
             about.Type = PluginType.General;
             about.VersionMajor = 1;
-            about.VersionMinor = 2;
-            about.Revision = 1;
+            about.VersionMinor = 3;
+            about.Revision = 0;
             about.MinInterfaceVersion = MinInterfaceVersion;
             about.MinApiRevision = MinApiRevision;
             about.ReceiveNotifications = ReceiveNotificationFlags.TagEvents;
